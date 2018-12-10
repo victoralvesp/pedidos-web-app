ea03a70b8059eb49365d8fcb3b36bc8e5077a6d7
victor <vpalves1@gmail.com>
2018-12-10 21:15:52 -0200

    Deploy para AWS

--- src/app/services/app.settings.ts
+++ src/app/services/app.settings.ts
@@ -5,7 +5,7 @@ constructor(private environment: string) {
         this.url_base = 'http://localhost:55416/';
     }
     if (this.environment === 'aws') {
-      this.url_base = 'http://ec2-18-231-93-130.sa-east-1.compute.amazonaws.com/';
+      this.url_base = 'http://ec2-18-231-93-130.sa-east-1.compute.amazonaws.com:100/';
     }
   }
 }
